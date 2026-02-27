212ddb5ea15e90df295effeb36210852fccc7c7c
mohamedmaghzaoui <mohamedmaghzaoui53@gmail.com>
2026-02-27 16:02:38 +0100

    login modal EDT-43

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,16 +1,25 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { RegisterModal } from './modals/RegisterModal';
-
+import { LoginModal } from './login/LoginModal';
+import { Register } from './register/Register';
 export default function Navbar() {
   const [isRegisterOpen, setIsRegisterOpen] = useState(false);
+  const [isLoginOpen, setIsLoginOpen] = useState(false);
 
   const showRegisterModal = () => {
     setIsRegisterOpen(true);
   };
 
+  const showLoginModal = () => {
+    setIsLoginOpen(true);
+  };
+
   const hideRegisterModal = () => {
     setIsRegisterOpen(false);
+  };
+
+  const hideLoginModal = () => {
+    setIsLoginOpen(false);
   };
 
   return (
@@ -23,9 +32,9 @@ export default function Navbar() {
         </div>
 
         <div className="flex-1">
-          <Link to="/login" className="btn btn-outline btn-sm mr-6">
+          <button onClick={() => showLoginModal()} className="btn btn-outline btn-sm mr-6">
             Login
-          </Link>
+          </button>
 
           <button onClick={() => showRegisterModal()} className="btn btn-success btn-sm">
             Register
@@ -43,7 +52,8 @@ export default function Navbar() {
         </div>
       </div>
 
-      <RegisterModal isOpen={isRegisterOpen} onClose={() => hideRegisterModal()} />
+      <Register isOpen={isRegisterOpen} onClose={() => hideRegisterModal()} />
+      <LoginModal isOpen={isLoginOpen} onClose={() => hideLoginModal()} />
     </>
   );
 }
