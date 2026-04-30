7310fee5e47e1fe0ade5504c6ef22a5da31af878
mohamedmaghzaoui <mohamedmaghzaoui53@gmail.com>
2026-04-30 23:01:38 +0200

    fix: default api url EDT-95

--- src/api/axios.tsx
+++ src/api/axios.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = import.meta.env.VITE_API_URL;
+const URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
 const api = axios.create({
   baseURL: URL,
   withCredentials: true,
