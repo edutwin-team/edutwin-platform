b9c82b88d1a3e34ade567a31e1a30049ceffef7e
mohamedmaghzaoui <mohamedmaghzaoui53@gmail.com>
2026-06-20 14:58:17 +0200

    feat: activate csrf protection EDT-103

--- src/api/axios.tsx
+++ src/api/axios.tsx
@@ -4,6 +4,21 @@ const URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
 const api = axios.create({
   baseURL: URL,
   withCredentials: true,
+  xsrfCookieName: 'csrftoken',
+  xsrfHeaderName: 'X-CSRFToken',
+});
+//csrf interceptor
+api.interceptors.request.use((config) => {
+  const csrfToken = document.cookie
+    .split('; ')
+    .find((row) => row.startsWith('csrftoken='))
+    ?.split('=')[1];
+
+  if (csrfToken) {
+    config.headers['X-CSRFToken'] = csrfToken;
+  }
+
+  return config;
 });
 
 export default api;
