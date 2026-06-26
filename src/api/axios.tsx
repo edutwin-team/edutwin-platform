821cb9995a9ac4169588d7e9243d38b8f767469e
mohamedmaghzaoui <mohamedmaghzaoui53@gmail.com>
2026-06-26 15:13:29 +0200

    hotfix: remove csrf interceptor EDT-000

--- src/api/axios.tsx
+++ src/api/axios.tsx
@@ -5,24 +5,6 @@ const URL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api';
 const api = axios.create({
   baseURL: URL,
   withCredentials: true,
-  xsrfCookieName: 'csrftoken',
-  xsrfHeaderName: 'X-CSRFToken',
-});
-
-api.interceptors.request.use((config) => {
-  let csrfToken = null;
-
-  if (typeof document !== 'undefined') {
-    const match = document.cookie.split('; ').find((row) => row.startsWith('csrftoken='));
-
-    csrfToken = match?.split('=')[1];
-  }
-
-  if (csrfToken) {
-    config.headers['X-CSRFToken'] = csrfToken;
-  }
-
-  return config;
 });
 
 export default api;
